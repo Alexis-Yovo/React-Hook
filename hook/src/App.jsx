7cbe92f5e2951cc14031a7c1a134d537a73091cd
Alexis <74179398+Alexis-Yovo@users.noreply.github.com>
2024-12-13 18:01:20 +0100

    first commit

---
+++ hook/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from "./Counter.jsx";
+import Input from "./Input.jsx";
+
+
+const App = () => {
+ 
+ return (
+  <>
+   <Counter />
+   <Input />
+  </>
+ )
+}
+
+export default App
